f0ea87a320074ae3426e1fdc52bf20a6e55f7f90
Nuno luciano <nunoluciano@yahoo.com>
2012-07-23 18:49:38 +0200

    Rev Theme XCL_Fadewhite
    Review and upload Theme version 2.2.1
    
    Signed-off-by: Nuno luciano <nunoluciano@yahoo.com>

--- public_html/themes/xcl_fadewhite/manifesto.ini.php
+++ public_html/themes/xcl_fadewhite/manifesto.ini.php
@@ -5,14 +5,14 @@
 Name="XCL Fade White"
 Depends=Legacy_RenderSystem,legacy
 Url="http://xoopscube.sourceforge.net/"
-Version="2.2a"
+Version="2.2.1"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
 Format="XOOPS2 Legacy Style"
 Author=Nuno Luciano
 ScreenShot="screenshot.png"
-Description="Header with slider. Layout with 2 columns and 2 bottom center-blocks."
+Description="Header with menu toggle active, image slider. Layout with 2 columns: left-block, module content and right-block. Bottom with three center-blocks."
 W3C=NG
 
 Licence="BSD"
