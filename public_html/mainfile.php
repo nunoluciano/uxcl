1693bffd0bbc77b0371a2674615b607bd7757d1c
Info Opensource <info.opensource@gmail.com>
2019-01-09 03:00:47 +0100

    Fix PHP 7.1.x

--- public_html/mainfile.php
+++ public_html/mainfile.php
@@ -25,7 +25,7 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
 //  ------------------------------------------------------------------------ //
 /**
- * Xoops is not installed, redirect to the installer 
+ * XCL is not installed, redirect to the installer 
  **/
 
 // XOOPS is not installed yet.
